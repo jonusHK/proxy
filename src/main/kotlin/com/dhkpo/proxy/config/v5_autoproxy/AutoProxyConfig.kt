4c6ce16cf4c62848234848d74cb246a9038c03e9
jonusHK <positipman@gmail.com>
2023-06-22 22:44:19 +0900

    Add: AspectJ 가 제공하는 포인트컷 적용

--- src/main/kotlin/com/dhkpo/proxy/config/v5_autoproxy/AutoProxyConfig.kt
+++ src/main/kotlin/com/dhkpo/proxy/config/v5_autoproxy/AutoProxyConfig.kt
@@ -6,6 +6,7 @@ import com.dhkpo.proxy.config.AppV2Config
 import com.dhkpo.proxy.config.v3_proxyfactory.advice.LogTraceAdvice
 import com.dhkpo.proxy.trace.logtrace.LogTrace
 import org.springframework.aop.Advisor
+import org.springframework.aop.aspectj.AspectJExpressionPointcut
 import org.springframework.aop.support.DefaultPointcutAdvisor
 import org.springframework.aop.support.NameMatchMethodPointcut
 import org.springframework.context.annotation.Bean
@@ -16,12 +17,38 @@ import org.springframework.context.annotation.Import
 @Import(AppV1Config::class, AppV2Config::class)
 class AutoProxyConfig {
 
-    @Bean
+//    @Bean
     fun advisor1(logTrace: LogTrace): Advisor {
 
         // pointcut
         val pointcut = NameMatchMethodPointcut()
         pointcut.setMappedNames("request*", "order*", "save*")
+
+        // advice
+        val advice = LogTraceAdvice(logTrace)
+        return DefaultPointcutAdvisor(pointcut, advice)
+    }
+
+//    @Bean
+    fun advisor2(logTrace: LogTrace): Advisor {
+
+        // pointcut
+        val pointcut = AspectJExpressionPointcut()
+        // AspectJ 가 제공하는 포인트컷 표현식
+        pointcut.expression = "execution(* com.dhkpo.proxy.app..*(..))"
+
+        // advice
+        val advice = LogTraceAdvice(logTrace)
+        return DefaultPointcutAdvisor(pointcut, advice)
+    }
+
+    @Bean
+    fun advisor3(logTrace: LogTrace): Advisor {
+
+        // pointcut
+        val pointcut = AspectJExpressionPointcut()
+        // AspectJ 가 제공하는 포인트컷 표현식
+        pointcut.expression = "execution(* com.dhkpo.proxy.app..*(..)) && !execution(* com.dhkpo.proxy.app..noLog(..))"
 
         // advice
         val advice = LogTraceAdvice(logTrace)
